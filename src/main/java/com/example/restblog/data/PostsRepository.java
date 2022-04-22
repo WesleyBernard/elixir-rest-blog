9c7c60546d6efad3f0c97be0f00c316824983e44
Wesley Bernard <wesley.bernard64@gmail.com>
2022-04-22 15:51:46 -0400

    refactoring

--- src/main/java/com/example/restblog/data/PostsRepository.java
+++ src/main/java/com/example/restblog/data/PostsRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Post, Long> {
-Post findByTitle(String title);
-List<Post> findByCategories(Category category);
+    List<Post> findByCategories(Category category);
 }
